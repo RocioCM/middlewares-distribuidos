b534ece5864edba9d84a9386de3e3a4d730746c9
Rocío Mena <rociocmena@gmail.com>
2022-10-20 19:56:52 -0300

    Horizontal scaling host

--- src/main/java/org/cloudsimplus/Example2.java
+++ src/main/java/org/cloudsimplus/Example2.java
@@ -67,16 +67,28 @@ import java.util.List;
  * @since CloudSim Plus 1.2.1
  */
 public class Example2 {
-    // TODO: armar infra definitiva.
-    private static final int HOSTS = 1;
-    private static final int HOST_PES = 6;
+    private static final int PE_MIPS = 1000; // in Million of Instructions per Second
 
-    private static final int VMS = 3;
+    private static final int VM_GROUPS = 3;
+    private static final int VMS_PER_GROUP = 5;
+    private static final int VMS = VM_GROUPS * VMS_PER_GROUP;
     private static final int VM_PES = 2;
+    private static final int VM_MIPS = 1000;
+    private static final int VM_BW = 1000;
+    private static final int VM_RAM = 512;
+    private static final int VM_STORAGE = 1000;
+
+    private static final int HOSTS = 1;
+    private static final int HOST_PES = VM_PES * VMS * 5; // Number of processing units
+    private static final long HOST_RAM = VM_RAM * VMS * 4; // in Megabytes
+    private static final long HOST_BW = VM_BW * VMS * 4; // in Megabits/s
+    private static final long HOST_STORAGE = VM_STORAGE * VMS * 4; // in Megabytes
 
     private static final int CLOUDLETS = 14;
     private static final int CLOUDLET_PES = 2;
     private static final int CLOUDLET_LENGTH = 10000;
+    private static final int CLOUDLET_FILE_SIZE = 1024;
+    private static final int CLOUDLET_OUTPUT_SIZE = 1024;
 
     private final CloudSim simulation;
     private DatacenterBrokerHeuristic broker0;
@@ -132,16 +144,13 @@ public class Example2 {
         List<Pe> peList = new ArrayList<>(HOST_PES);
         // List of Host's CPUs (Processing Elements, PEs)
         for (int i = 0; i < HOST_PES; i++) {
-            peList.add(new PeSimple(1000, new PeProvisionerSimple()));
+            peList.add(new PeSimple(PE_MIPS, new PeProvisionerSimple()));
         }
 
-        final long ram = 2048 * 2; // in Megabytes
-        final long bw = 10000; // in Megabits/s
-        final long storage = 1000000; // in Megabytes
         ResourceProvisioner ramProvisioner = new ResourceProvisionerSimple();
         ResourceProvisioner bwProvisioner = new ResourceProvisionerSimple();
         VmScheduler vmScheduler = new VmSchedulerTimeShared();
-        Host host = new HostSimple(ram, bw, storage, peList);
+        Host host = new HostSimple(HOST_RAM, HOST_BW, HOST_STORAGE, peList);
         host
                 .setRamProvisioner(ramProvisioner)
                 .setBwProvisioner(bwProvisioner)
@@ -154,12 +163,16 @@ public class Example2 {
      */
     private List<Vm> createVms() {
         final List<Vm> list = new ArrayList<>(VMS);
-        for (int v = 0; v < VMS; v++) {
-            Vm vm = new VmSimple(v, 1000, VM_PES)
-                    .setRam(512).setBw(1000).setSize(10000)
-                    .setCloudletScheduler(new CloudletSchedulerSpaceShared());
 
-            list.add(vm);
+        for (int g = 0; g < VM_GROUPS; g++) {
+            int factor = g + 1;
+            for (int v = 0; v < VMS_PER_GROUP; v++) {
+                Vm vm = new VmSimple(g * VMS_PER_GROUP + v, VM_MIPS * factor, VM_PES * factor)
+                        .setRam(VM_RAM * factor).setBw(VM_BW * factor).setSize(VM_STORAGE * factor)
+                        .setCloudletScheduler(new CloudletSchedulerSpaceShared());
+
+                list.add(vm);
+            }
         }
 
         return list;
@@ -174,8 +187,8 @@ public class Example2 {
         // UtilizationModel utilization = new UtilizationModelDynamic(30);
         for (int c = 0; c < CLOUDLETS; c++) {
             Cloudlet cloudlet = new CloudletSimple(c, CLOUDLET_LENGTH, CLOUDLET_PES)
-                    .setFileSize(1024)
-                    .setOutputSize(1024)
+                    .setFileSize(CLOUDLET_FILE_SIZE)
+                    .setOutputSize(CLOUDLET_OUTPUT_SIZE)
                     .setUtilizationModel(utilization);
             list.add(cloudlet);
         }
